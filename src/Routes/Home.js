83519abe1ba0558e02c689aa2d6920317d3b4b5a
hoffmanaiden <hoffman.aiden@gmail.com>
2022-06-29 15:15:11 -0400

    text change

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
       <div className="text-box">
         Hello!<br />
         My name is Aiden & I'm a Full Stack Developer with a focus on React, Node, & AWS.
-        I love to learn, build, and troubleshoot. Currently obsessed with the <a target="_blank" href="https://github.com/pmndrs">Poimandres</a> community.
+        I love to learn, build, and troubleshoot.
       </div>
       {/* <ToggleLeva/> */}
     </div>
